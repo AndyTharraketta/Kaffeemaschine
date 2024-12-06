4074451443b7a8d111b4da2bbb1d0fa4ae1980c9
Tharraketta <andreas.tharraketta@training.lug-ag.de>
2024-12-06 10:15:50 +0100

    task 1

---
+++ Kaffeemaschine.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kaffeemaschine
+{
+    internal class Kaffeemaschine
+    {
+        private int wasserstand;
+        private int bohnenmenge;
+
+        public static int maxWasserstand = 2000;        // Max Wasserfüllmenge in ml
+        public static int maxBohnenmenge = 400;         // Max Bohnenfüllmenge in g
+
+        public int Wasserstand { get => wasserstand; set => wasserstand = value; }      // via Rechtsklick eingefügt
+        public int Bohnenmenge { get => bohnenmenge; set => bohnenmenge = value; }      // via Rechtsklick eingefügt
+
+        public Kaffeemaschine(int wasserstand, int bohnenmenge)
+        {
+            this.wasserstand = wasserstand;
+            this.bohnenmenge = bohnenmenge;
+        }
+
+        public Kaffeemaschine()
+        {
+        }
+
+        public int WasserstandAuffuellen()
+        {
+            return maxWasserstand - wasserstand;
+        }
+        
+        public int BohnenmengeAuffuellen()
+        {
+            return maxBohnenmenge - bohnenmenge;
+        }
+
+
+
+
+
+    }
+}
